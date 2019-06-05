fe56a8cb52dad08936f822423f3967fdec70fbd7
V. Alfonso Pérez Espino <taxgype1@gmail.com>
2019-06-04 20:49:24 -0500

    Prueba commit

---
+++ src/Logica/MeteoroClass.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+import Logica.MeteoroClass;
+import java.nio.file.Files;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.scene.layout.AnchorPane;
+
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author VAPESIN
+ */
+public class MeteoroClass extends Thread {
+
+    private  Circle meteoro;
+    private AnchorPane fondo;
+    private Random random;
+    
+    public MeteoroClass( AnchorPane fondo) {
+        this.fondo = fondo;
+    random = new Random();
+    //    this.meteoro = new Circle(50, 50, 20);
+    /*
+    Se multiplica x2, porque le estamos restanto ya la altura de la ventana, por lo tanto, en la coordenada resultante, jamás llegará en Y a la coordenada maxima,
+    así que si multiplicamos x2 el rango de generacion, se cubre esa zona faltante.
+    */
+        this.meteoro = new Circle( -50,-50 -RecursosGlobales.altoMenu + random.nextInt((int) (RecursosGlobales.altoMenu * 2)), 10);
+            Platform.runLater(()->{
+ 
+          
+                fondo.getChildren().add(0,this.meteoro);
+
+            }); 
+        this.setDaemon(true);
+    }
+    
+    @Override
+    public void run(){
+        
+    
+      //  if(meteoro!=null)
+        while(meteoro.getCenterX() + RecursosGlobales.aumentoX < RecursosGlobales.largoMenu
+                    && meteoro.getCenterY() + RecursosGlobales.aumentoY < RecursosGlobales.altoMenu){
+            
+            Platform.runLater(()->{
+                 meteoro.setCenterX(meteoro.getCenterX() + RecursosGlobales.aumentoX);
+                 meteoro.setCenterY(meteoro.getCenterY() + RecursosGlobales.aumentoY);
+            });
+
+            try {
+                sleep(RecursosGlobales.velocidadLluvia);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MeteoroClass.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Platform.runLater(()->{
+        
+            fondo.getChildren().remove(this.meteoro);
+
+        });
+        
+    }
+    
+}
---
+++ src/Logica/RecursosGlobales.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author VAPESIN
+ */
+public class RecursosGlobales {
+
+
+    public static boolean lluviaMeteoros = true; 
+    public static double aumentoX= 3;
+    public static double aumentoY= 3;
+    public static long velocidadLluvia = 2;
+    public static double largoMenu=790;
+    public static double altoMenu=570;
+    
+}
