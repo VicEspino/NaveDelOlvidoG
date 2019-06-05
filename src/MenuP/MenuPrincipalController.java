990490e46b84e7c1e22f6fd0f65b397b60cb3bdd
V. Alfonso Pérez Espino <taxgype1@gmail.com>
2019-06-04 22:31:13 -0500

    Creación de ventana para el menú, se pretende volverla componente para añadirla como hijo a la ventana principal, para ser un tipo de ventana mini deslizante.

--- src/MenuP/MenuPrincipalController.java
+++ src/MenuP/MenuPrincipalController.java
@@ -8,6 +8,7 @@ package MenuP;
 import Logica.MeteoroClass;
 import Logica.RecursosGlobales;
 import com.jfoenix.controls.JFXSlider;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -15,7 +16,11 @@ import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
@@ -24,10 +29,12 @@ import javafx.scene.input.SwipeEvent;
 import javafx.scene.input.TouchEvent;
 import javafx.scene.input.ZoomEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -102,7 +109,16 @@ public class MenuPrincipalController implements Initializable {
     }
 
     @FXML
-    private void btnOpciones_OnAction(ActionEvent event) {
+    private void btnOpciones_OnAction(ActionEvent event) throws IOException {
+        
+       /* Parent root = FXMLLoader.load(getClass().getResource("/ConfiguracionVentana/VentanaConfiguracion.fxml"));
+        
+        VBox v = new VBox();
+        v.getChildren().addAll(root,(Node) event.getSource()  );
+         Stage newStage = new Stage();
+        newStage = (Stage)( ((Node) (event.getSource() ) ).getScene().getWindow() );
+        newStage.setScene(new Scene(v));
+        */
     }
 
     @FXML
