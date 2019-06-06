2f0fdabf3ada38d060c828c34289731bcfef03fe
VicEspino <taxgype1@gmail.com>
2019-06-05 19:28:18 -0500

    Se agregó ventana de configuración deslizante, falta enlazar slider de volumen.

--- src/ConfiguracionVentana/VentanaConfiguracionController.java
+++ src/ConfiguracionVentana/VentanaConfiguracionController.java
@@ -5,28 +5,74 @@
  */
 package ConfiguracionVentana;
 
+import Logica.RecursosGlobales;
+import com.jfoenix.controls.JFXSlider;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
  *
  * @author VAPESIN
  */
-public class VentanaConfiguracionController implements Initializable {
+public class VentanaConfiguracionController extends AnchorPane{
 
     @FXML
     private Button btnCerrar;
+    @FXML
+    private AnchorPane AnchorPane;
+    private AnchorPane fondo;
+    
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    FXMLLoader fxmlLoader;
+    @FXML
+    private JFXSlider sliderMusica;
+    
+    public VentanaConfiguracionController(AnchorPane fondo) {
+       this.fondo = fondo;
+    
+        try {
+            fxmlLoader= new FXMLLoader(
+                getClass().getResource("VentanaConfiguracion.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+            fxmlLoader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(VentanaConfiguracionController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+         sliderMusica.valueProperty().addListener(
+               (observable, oldValue, newValue) -> {
+        
+                       RecursosGlobales.music.volumeProperty().set(newValue.doubleValue()/100);
+                   //    RecursosGlobales.music.
+                       // RecursosGlobales.music.getVolume();
+
+                   //});
+       } );
+                       
+    }
+
+    @FXML
+    private void btnCerrar_Click(ActionEvent event) {
+        //AnchorPane p = fxmlLoader.getRoot();
+    //    p.getChildren().remove(p);
+    
+    fondo.getChildren().remove(this);
+    }
+
+    
+    
+  
     
 }
